b56b2844973f3b3c550c6b2576eadc4a4ac5904e
Kingslet Effiong <kingsleyeffiong642@gmail.com>
2024-07-27 08:33:40 +0100

    updated

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/fireba
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+// const firebaseConfig = {
+
+//   apiKey: "AIzaSyDRDo-m0Lsd5hbdfYMywlc3WUbUtT-bwLY",
+
+//   authDomain: "church-website-fffe9.firebaseapp.com",
+
+//   projectId: "church-website-fffe9",
+
+//   storageBucket: "church-website-fffe9.appspot.com",
+
+//   messagingSenderId: "317957225784",
+
+//   appId: "1:317957225784:web:38ee216b186dfb66d755cd",
+
+//   measurementId: "G-TH3KBDVN1Z"
+
+// };
+
+
 const firebaseConfig = {
 
   apiKey: "AIzaSyA0h7oXXRVr4VAY5TJAIoq8O-gdXh25xq0",
@@ -27,7 +46,6 @@ const firebaseConfig = {
   measurementId: "G-L2WK5GH6W0"
 
 };
-
 
 
 // Initialize Firebase
